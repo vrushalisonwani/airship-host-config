ad2844a158d2823af42bbfbe405d97ccd4c2efa0
Vrushali Sonwani <er.vrush@gmail.com>
2020-06-17 19:05:48 +0530

    Update dynamic_inventory.py

--- airship-host-config/inventory/dynamic_inventory.py
+++ airship-host-config/inventory/dynamic_inventory.py
@@ -4,8 +4,8 @@ import os
 import sys
 import argparse
 import time
-import kubernetes.client
 import base64
+import kubernetes.client
 from kubernetes.client.rest import ApiException
 import yaml
 
@@ -21,7 +21,6 @@ class KubeInventory(object):
 
         self.api_instance = kubernetes.client.CoreV1Api(kubernetes.config.load_incluster_config())
         if self.args.list:
-#            self.inventory = self.kube_inventory()
             self.kube_inventory()
         elif self.args.host:
             # Not implemented, since we return _meta info `--list`.
@@ -35,19 +34,51 @@ class KubeInventory(object):
     # Kube driven inventory
     def kube_inventory(self):
         self.inventory = {"group": {"hosts": [], "vars": {}}, "_meta": {"hostvars": {}}}
-        self.set_ssh_keys()
         self.get_nodes()
 
-    def set_ssh_keys(self):
-        self.inventory["group"]["vars"]["ansible_ssh_user"] = os.environ.get("USER") if "USER" in os.environ else "kubernetes"
-        if "PASS" in os.environ:
-            self.inventory["group"]["vars"]["ansible_ssh_pass"] = os.environ.get("PASS")
+    # Sets the ssh username and password using the pod environment variables
+    def _set_ssh_keys(self, labels, node_internalip):
+        namespace = ""
+        if "SECRET_NAMESPACE" in os.environ:
+            namespace = os.environ.get("SECRET_NAMESPACE")
         else:
-            self.inventory["group"]["vars"][
-                "ansible_ssh_private_key_file"
-            ] = "~/.ssh/id_rsa"
+            namespace = "default"
+        if "secret" in labels.keys():
+            try:
+                secret_value = self.api_instance.read_namespaced_secret(labels["secret"], namespace)
+            except ApiException as e:
+                return False
+            if "username" in secret_value.data.keys():
+                username = (base64.b64decode(secret_value.data['username'])).decode("utf-8")
+                self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_user"] = username
+            elif "USER" in os.environ:
+                self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_user"] = os.environ.get("USER")
+            else:
+                self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_user"] = 'kubernetes'
+            if "password" in secret_value.data.keys():
+                password = (base64.b64decode(secret_value.data['password'])).decode("utf-8")
+                self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_pass"] = password
+            elif "PASS" in os.environ:
+                self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_pass"] =  os.environ.get("PASS")
+            else:
+                self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_pass"] = 'kubernetes'
+        else:
+            return False
+        return True
+
+    def _set_default_ssh_keys(self, node_internalip):
+        if "USER" in os.environ:
+            self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_user"] = os.environ.get("USER")
+        else:
+            self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_user"] = 'kubernetes'
+        if "PASS" in os.environ:
+            self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_pass"] =  os.environ.get("PASS")
+        else:
+            self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_pass"] = 'kubernetes'
         return
 
+    # Gets the Kubernetes nodes labels and annotations and build the inventory
+    # Also groups the kubernetes nodes based on the labels and annotations
     def get_nodes(self):
         #label_selector = "kubernetes.io/role="+role
 
@@ -55,9 +86,6 @@ class KubeInventory(object):
             nodes = self.api_instance.list_node().to_dict()[
                 "items"
             ]
-            #nodes = self.api_instance.list_node(label_selector=label_selector).to_dict()[
-            #   "items"
-            #]
         except ApiException as e:
             print("Exception when calling CoreV1Api->list_node: %s\n" % e)
 
@@ -72,57 +100,28 @@ class KubeInventory(object):
             self.inventory["group"]["hosts"].append(node_internalip)
 
             self.inventory["_meta"]["hostvars"][node_internalip] = {}
+            if not self._set_ssh_keys(node["metadata"]["labels"], node_internalip):
+                self._set_default_ssh_keys(node_internalip)
             for key, value in node["metadata"]["annotations"].items():
                 self.inventory["_meta"]["hostvars"][node_internalip][key] = value
             for key, value in node["metadata"]["labels"].items():
                 self.inventory["_meta"]["hostvars"][node_internalip][key] = value
                 if key in interested_labels_annotations:
-                    if value not in self.inventory.keys():
-                        self.inventory[value] = {"hosts": [], "vars": {}}
-                    if node_internalip not in self.inventory[value]["hosts"]:
-                        self.inventory[value]["hosts"].append(node_internalip)
-                if key == "secret":
-                    secret_value = self.api_instance.read_namespaced_secret(value,SECRET_NAMESPACE)
-                    if "username" in secret_value.data.keys():
-#                        import pdb;pdb.set_trace()
-                        username = (base64.b64decode(secret_value.data['username'])).decode("utf-8")
-                        self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_user"] = username
-                    elif "USER" in os.environ:
-                        self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_user"] = os.environ.get("USER")
-                    else:
-                        self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_user"] = 'kubernetes'
-
-                    if "password" in secret_value.data.keys():
-                        password = (base64.b64decode(secret_value.data['password'])).decode("utf-8")
-                        self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_pass"] = password
-                    elif "PASS" in os.environ:
-                        self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_pass"] =  os.environ.get("PASS")
-                    else:
-                        self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_pass"] = 'kubernetes'
-                else:
-                    if "USER" in os.environ:
-                        self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_user"] = os.environ.get("USER")
-                    else:
-                        self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_user"] = 'kubernetes'
-                    if "PASS" in os.environ:
-                        self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_pass"] =  os.environ.get("PASS")
-                    else:
-                        self.inventory["_meta"]["hostvars"][node_internalip]["ansible_ssh_pass"] = 'kubernetes'
+                    if key+'_'+value not in self.inventory.keys():
+                        self.inventory[key+'_'+value] = {"hosts": [], "vars": {}}
+                    if node_internalip not in self.inventory[key+'_'+value]["hosts"]:
+                        self.inventory[key+'_'+value]["hosts"].append(node_internalip)
             for key, value in node['status']['node_info'].items():
                 self.inventory["_meta"]["hostvars"][node_internalip][key] = value
                 if key in interested_labels_annotations:
-                    if value not in self.inventory.keys():
-                        self.inventory[value] = {"hosts": [], "vars": {}}
-                    if node_internalip not in self.inventory[value]["hosts"]:
-                        self.inventory[value]["hosts"].append(node_internalip)
-            #self.inventory["_meta"]["hostvars"][node_internalip] = {}
+                    if key+'_'+value not in self.inventory.keys():
+                        self.inventory[key+'_'+value] = {"hosts": [], "vars": {}}
+                    if node_internalip not in self.inventory[key+'_'+value]["hosts"]:
+                        self.inventory[key+'_'+value]["hosts"].append(node_internalip)
             self.inventory["_meta"]["hostvars"][node_internalip][
                 "kube_node_name"
             ] = node["metadata"]["name"]
-            #self.inventory["_meta"]["hostvars"][node_internalip]["architecture"] = node['status']['node_info']['architecture']
-            #self.inventory["_meta"]["hostvars"][node_internalip]["kernel_version"] = node['status']['node_info']['kernel_version']
-
-#        return self.inventory
+        return
 
     def empty_inventory(self):
         return {"_meta": {"hostvars": {}}}
@@ -137,3 +136,4 @@ class KubeInventory(object):
 
 # Do computer.
 KubeInventory()
+
